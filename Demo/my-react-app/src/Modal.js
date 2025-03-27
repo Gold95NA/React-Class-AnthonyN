6b978a283c8cd10af5672f2404f0af5152724752
Gold95NA <156334092+Gold95NA@users.noreply.github.com>
2025-03-26 21:11:19 -0400

    commit

---
+++ Demo/my-react-app/src/Modal.js
@@ -0,0 +1,10 @@
+export const Modal = () => {
+    return (
+        <div className="modal">
+            <p>Did you complete this task?</p>
+            <button onClick={() =>{}}className="btn btn-highlight">Cancel</button>
+            <button onClick={() =>{}}className="btn btn-highlight">Confirm</button>
+        </div>
+    );
+}
+
