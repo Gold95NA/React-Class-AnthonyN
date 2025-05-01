7e2a784a7877bb700d2d0a9c136bfaeb8256d563
Gold95NA <156334092+Gold95NA@users.noreply.github.com>
2025-04-30 21:57:50 -0400

    homework 1

---
+++ my-react-app/src/components/TodoItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const TodoItem = ({ task, onMarkDone }) => {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center',      
+      flexDirection: 'column', 
+      gap: '20px',     
+      marginBottom: '1rem', 
+      padding: '1rem', 
+      border: '1px solid #ccc'
+      }}> 
+      <p style={{ margin: 0 }}>{task.text}</p>
+      <button onClick={() => onMarkDone(task)} disabled={task.completed}>
+        {task.completed ? 'Completed' : 'Done'}
+      </button>
+    </div>
+  );  
+};
+
+export default TodoItem;
